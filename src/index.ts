dc211042bd6b8669b73de3261d676468032841ec
Dev Catalin <20538711+devcatalin@users.noreply.github.com>
2024-01-25 16:00:18 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -149,7 +149,7 @@ if (await which("kubectl-testkube", { nothrow: true })) {
 
   const existingTestkubePath = toolCache.find("kubectl-testkube", params.version, `${system}-${architecture}`);
 
-  console.log("existingTestkubePath", existingTestkubePath);
+  console.log("existingTestkubePath:", existingTestkubePath);
 
   if (!existingTestkubePath || existingTestkubePath.length === 0) {
     process.stdout.write(`Downloading the artifact from "${artifactUrl}"...\n`);
